311d5d9272947deab62bae981c8aceb3562b2cfa
Omolade Ikumapayi <oikumapa@uccs.edu>
2022-03-02 22:38:13 -0700

    commit

--- code/Mac_Response_time.py
+++ code/Mac_Response_time.py
@@ -34,12 +34,10 @@ class Response_time:
                 num_frame.append(math.ceil((float(row[1]) + 3 +1)/8)) #using the first mac profile
                 
         modul = [(DLC[i] + 4) % 8 for i in range(len(DLC))] # mac &fv =4
-        #print(cmax)
         cmax = [ (55 + (10 * 8)) * tbit for i in range(len(DLC)) ] #C_Dmax
         cmac =[ (55 + 10 * modul[i]) * tbit for i in range(len(DLC)) ]  #C_(D+A+F)_mod Dmax
 
         #appends the result to the transmission time of each messages 
-
         for i in range(len(DLC)):
             n_tx = (num_frame[i]-1) * cmax[i] +cmac[i]
             TX.append(n_tx)
@@ -80,9 +78,9 @@ class Response_time:
                 Blocking_m= max(lp)
 
         return Blocking_m
-
-    def get_qd_m(self,p, tbit, B, q, w, prev_w=None, j=0):
         
+    #calculates the queuing delay
+    def get_qd_m(self,p, tbit, B, q, w, prev_w=None, j=0):
         
         if w ==prev_w: return  prev_w
         #initialise blocking messages to be zero
@@ -92,18 +90,16 @@ class Response_time:
         qd = 0
         
         #follows the use case to derive the worst case queuing delay
-        
         for r in range(len(self.period)):
             
             if max(self.priority)==p:
                 qd = B + q * self.TX[r]
                 
-            v=v+1       
+            v=v+1   #nextIndex_holder    
                 
             if self.priority[r]< self.priority[p]:
                 
                 val = (w+j+tbit) / self.period[v-1]
-                #print(math.ceil(val))
                 sum_val += math.ceil(val) * self.TX[v-1] 
                 
             qd = B + ((q % self.num_frame[p]) *self.cmax[p]) + (math.floor(q/self.num_frame[p])* self.TX[p]) +sum_val
@@ -113,41 +109,31 @@ class Response_time:
 
     def get_Response_time(self,p,Q,B,tbit):
         w=0
-        Response_Time =0
+        Response_Time, f_resp,l_resp =0,0,0
         r=[]
         #to derive the number of instances of the message 
-        
-        
         for q in range(int(Q)):
             init_w = B + math.floor(q/self.num_frame[p]) * self.TX[p]
             
             if q == 0:
                 prev_inst = self.get_qd_m(p,tbit,B,q,init_w)
-                #print(prev_inst)
-                
-                
-                Response_Time = round((prev_inst - (math.floor(q/self.num_frame[p]) *self.period[p]) + (math.floor(((q %self.num_frame[p])+1))/self.num_frame[p]) *self.cmac[p] +((1- math.floor((q %self.num_frame[p])+1)/self.num_frame[p])*self.cmax[p])),6)
+                f_resp= round((prev_inst - (math.floor(q/self.num_frame[p]) *self.period[p]) + (math.floor(((q %self.num_frame[p])+1))/self.num_frame[p]) *self.cmac[p] +((1- math.floor((q %self.num_frame[p])+1)/self.num_frame[p])*self.cmax[p])),6)
                     
             elif q > 0:
                 newq=q-1
                 w= self.get_qd_m(p,tbit,B,newq,B) + self.TX[p]
                 
-                
-                
                 new_inst =self.get_qd_m(p,tbit,B,q,w)
-                #print("t",new_inst)
-                
-                
-                #print(cmac[p])
-            
-                Response_Time = round((new_inst - (math.floor(q/self.num_frame[p]) *self.period[p]) + (math.floor(((q %self.num_frame[p])+1))/self.num_frame[p]) *self.cmac[p] +((1- math.floor((q %self.num_frame[p])+1)/self.num_frame[p])*self.cmax[p])),6)
-            
+                l_resp = round((new_inst - (math.floor(q/self.num_frame[p]) *self.period[p]) + (math.floor(((q %self.num_frame[p])+1))/self.num_frame[p]) *self.cmac[p] +((1- math.floor((q %self.num_frame[p])+1)/self.num_frame[p])*self.cmax[p])),6)
+            if f_resp > l_resp:
+                Response_Time = f_resp
+            else:
+                Response_Time = l_resp
              
         return Response_Time
     #the code works whether the file is sorted according to the priortiy or not,this method sorts it to a csv file 
     def sort_file_to_csv(self,res):
-        CURRENT_DIR = os.path.dirname(__file__)
-        file_path2 = os.path.join(CURRENT_DIR,'file_test_result.csv')
+        file_path2 = ""
         with open(file_path2, "w",newline="") as output:
             writer = csv.writer(output)
             for  row in range(len(self.period)):
@@ -176,7 +162,6 @@ def main():
         B = resp.get_Blocking_m(p)
         t = resp.num_frame[p] * resp.TX[p]
         length_m = resp.get_length_m(p, B, t)
-        
     
         Q = math.ceil(length_m /resp.period[p]) + (math.ceil(length_m /resp.period[p]) * resp.num_frame[p])
         
