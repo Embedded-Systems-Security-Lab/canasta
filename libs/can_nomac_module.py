aa5fdde06a8108eeb48d29a368de51e652350625
Omolade <oikumapa@uccs.edu>
2023-02-06 11:26:45 -0700

    code library

--- libs/can_nomac_module.py
+++ libs/can_nomac_module.py
@@ -7,12 +7,6 @@ def can_result(priority, period, TX, DLC, tbit):
     for p in range(len(priority)):
         
         B= resp.get_Blocking_m(p)
-        #print(B)
-        
         length_m=resp.get_length_m(p,TX,B,TX[p])
-        #print(length_m)
         Q=math.ceil(length_m /period[p])
-        #print(Q)
-        
         result.append(resp.get_Response_time(p,Q,B))
-        #get_Response_time(p,Q,B)
